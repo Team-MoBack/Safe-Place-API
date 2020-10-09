846bcec30664b5f6d112c18147112c4c58de7bcb
김재구 <rlaworn1993@gmail.com>
2020-10-09 21:38:38 +0900

    init project

---
+++ moBack/src/main/java/com/moBack/springboot/moBack/MoBackApplication.java
@@ -0,0 +1,13 @@
+package com.moBack.springboot.moBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoBackApplication.class, args);
+	}
+
+}
---
+++ moBack/src/test/java/com/moBack/springboot/moBack/MoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moBack.springboot.moBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
