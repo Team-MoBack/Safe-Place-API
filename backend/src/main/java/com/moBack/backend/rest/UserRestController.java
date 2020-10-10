660e1a74908b14e3de66949b6b64e951fdcfa4db
김재구 <rlaworn1993@gmail.com>
2020-10-10 14:39:45 +0900

    changed custom status code for error

--- backend/src/main/java/com/moBack/backend/rest/UserRestController.java
+++ backend/src/main/java/com/moBack/backend/rest/UserRestController.java
@@ -3,12 +3,14 @@ package com.moBack.backend.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.moBack.backend.entity.User;
 import com.moBack.backend.service.UserService;
@@ -33,9 +35,22 @@ public class UserRestController {
 	public User getUser(@PathVariable int id) {
 		User user = userService.findById(id);
 		if (user == null) {
-			throw new RuntimeException("user id not found - " + id);
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "user id not found - " + id);
 		}
 		return user;
+	}
+	
+	@PostMapping("/login")
+	public User authenticate(@RequestBody User user){
+		List<User> users = userService.findAll();
+		String email = user.getEmail();
+		String pw = user.getPassword();
+		for (User temp : users) {
+			if (email.equals(temp.getEmail()) && pw.equals(temp.getPassword())) {
+				return user;
+			}
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "email or password is not matched");
 	}
 	
 	@PostMapping("/register")
@@ -44,7 +59,7 @@ public class UserRestController {
 		List<User> users = userService.findAll();
 		for (User temp : users) {
 			if (temp.getEmail().equals(user.getEmail())) {
-				throw new RuntimeException("user id is duplicated - " + user.getId());
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "user id is duplicated - " + user.getId());
 			}
 		}
 		userService.save(user);
