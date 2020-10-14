44bbc65420af5be33998bef97c3017558c9f913b
김재구 <rlaworn1993@gmail.com>
2020-10-14 18:45:39 +0900

    [ENV] Added session

--- SafePlaceAPI/src/main/java/com/moBack/backend/config/SecurityConfig.java
+++ SafePlaceAPI/src/main/java/com/moBack/backend/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.moBack.backend.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 
 import com.moBack.backend.service.TestUserService;
 
@@ -14,31 +18,40 @@ import com.moBack.backend.service.TestUserService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
      @Autowired TestUserService userService;
- 
+    
      @Override
      protected void configure(HttpSecurity http) throws Exception {
           http
                .csrf().disable()
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER)
+                    .and()
                .authorizeRequests()
+                    .antMatchers("/user/login").permitAll()
+                    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                     .antMatchers("/user").hasAuthority("USER")
                     .antMatchers("/admin").hasAuthority("ADMIN")
                     .anyRequest().authenticated()
                     .and()
-             //  .formLogin()
-            //        .and()
+//               .formLogin()
+//                    .and()
                .logout()
-               ;
+               ;              
      }
-
+    
      @Override
      protected void configure(AuthenticationManagerBuilder auth) throws Exception {
           auth.userDetailsService(userService)
                .passwordEncoder(userService.passwordEncoder());
      }
-     
+    
      @Bean
      @Override
      public AuthenticationManager authenticationManagerBean() throws Exception {
           return super.authenticationManagerBean();
+     }
+    
+     @Bean
+     public HttpSessionStrategy httpSessionStrategy() {
+               return new HeaderHttpSessionStrategy();
      }
 }
