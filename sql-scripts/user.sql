801fd4216e14daa4651ddf3a85c271e454f723dd
김재구 <rlaworn1993@gmail.com>
2020-10-15 18:50:02 +0900

    Update user.sql

--- sql-scripts/user.sql
+++ sql-scripts/user.sql
@@ -1,24 +1,17 @@
 USE `moBack`;
 
---
--- Table structure for table `users`
---
-
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`id` int NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(50) NOT NULL,
+    `last_name` varchar(50) NOT NULL,
     `email` varchar(50) NOT NULL,
-    `password` char(68) NOT NULL,
-    `name` varchar(50) NOT NULL,
-    `longitude` double(13,10),
-    `latitude` double(13,10),
-    
+    `password` varchar(50) NOT NULL,
+
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-INSERT INTO `user` 
-VALUES 
-(1,'jaegu88@naver.com','1234','kim jaegu',0,0),
-(2,'mary123@kakao.com','1234','mary kim',36.4344,154.12323),
-(3,'susan111@gmail.com','1234','susan lee',44.2232,12.32323);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+-- INSERT INTO `user` 
+-- VALUES 
+-- (1,'jaegu88@naver.com','1234','kim jaegu',0,0),
+-- (2,'mary123@kakao.com','1234','mary kim',36.4344,154.12323),
+-- (3,'susan111@gmail.com','1234','susan lee',44.2232,12.32323);
