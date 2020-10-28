b12d9064fe32a13d2541e6cee5770d445c593629
김재구 <rlaworn1993@gmail.com>
2020-10-28 18:55:18 +0900

    updated .travis.yml

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #docker login -u $DOCKER_USER -p $DOCKER_PASS
-echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
 docker build --tag kimwithglasses/safe-place-db:0.0.1 . && docker push kimwithglasses/safe-place-db
 cd SafePlaceAPI && ./gradlew bootJar && docker build --tag kimwithglasses/safe-place-api:0.0.1 . && docker push kimwithglasses/safe-place-api
