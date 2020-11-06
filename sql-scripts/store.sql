8432c99a10a1ae790624c652348baf7f5d6ebb8a
김재구 <rlaworn1993@gmail.com>
2020-11-06 22:36:53 +0900

    removed dummy rows for store

--- sql-scripts/store.sql
+++ sql-scripts/store.sql
@@ -10,9 +10,3 @@ CREATE TABLE `store` (
     `latitude` double(13,10),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `store` 
-VALUES
-(1,'Two Some Place','박준현','카페',36.333,64.3434),
-(2,'Start Bucks Dongsungro','홍길동','카페',36.4344,154.12323),
-(3,'Angelinus','김자이구','카페',44.2232,12.32323);
