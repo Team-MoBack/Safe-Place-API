1146169af20751132db85b399ea835b481204bd7
김재구 <rlaworn1993@gmail.com>
2020-11-10 21:17:49 +0900

    cleaned jwt project

--- jwtDemo/src/main/java/com/jwtDemo/jwtDemo/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jwtDemo.jwtDemo.member;
-
-import java.util.Optional;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
-}
